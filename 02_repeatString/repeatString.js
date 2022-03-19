d5f7c1d3f8b97aee54a110d0111b484ca57cdba0
Evan Buckmaster <evanbuckmaster@gmail.com>
2022-03-19 08:27:38 -0400

    completed hello world and 1st exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
-};
+
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let combinedWord = '';
+    for (let i = 0; i < num; i++) {
+        combinedWord+=string;
+    }
+return combinedWord;
+}
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
