a33a5cbb337fc8ff6dbc4f74f0584e024618a512
Evan B <evanbuckmaster@gmail.com>
2022-03-19 19:12:53 -0400

    completed 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...items) {
+    //three dots is rest, so items is now an array of items to be removed
+    //I've got two arrays now
+    //for every value in items array, check if the value also exists in
+    //mainArray. if it does, remove it from mainArray
+    
+    for (let i = 0; i < items.length; i++) {
+        for (let j = 0; j < mainArray.length; j++) {
+            //console.log(`${items[i]} - ${mainArray[j]}`);
+            if (items[i] === mainArray[j]) {
+                mainArray.splice(j, 1);
+            }
+        }
+    }   
+    return mainArray;
+    };
 
-};
+    let result = removeFromArray([2,4,10], 10);
+console.log(result);
 
 // Do not edit below this line
 module.exports = removeFromArray;
