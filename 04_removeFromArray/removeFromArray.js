7f98f44972824d1c3317d04a96af38e08aaa41b3
Evan Buckmaster <evanbuckmaster@gmail.com>
2022-03-24 11:31:44 -0400

    re-wrote using filter and arrow function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,22 +1,34 @@
-const removeFromArray = function(mainArray, ...items) {
-    //three dots is rest, so items is now an array of items to be removed
-    //I've got two arrays now
-    //for every value in items array, check if the value also exists in
-    //mainArray. if it does, remove it from mainArray
+// const removeFromArray = function(mainArray, ...items) {
+//     //three dots is rest, so items is now an array of items to be removed
+//     //I've got two arrays now
+//     //for every value in items array, check if the value also exists in
+//     //mainArray. if it does, remove it from mainArray
     
-    for (let i = 0; i < items.length; i++) {
-        for (let j = 0; j < mainArray.length; j++) {
-            //console.log(`${items[i]} - ${mainArray[j]}`);
-            if (items[i] === mainArray[j]) {
-                mainArray.splice(j, 1);
-            }
-        }
-    }   
-    return mainArray;
-    };
+//     for (let i = 0; i < items.length; i++) {
+//         for (let j = 0; j < mainArray.length; j++) {
+//             //console.log(`${items[i]} - ${mainArray[j]}`);
+//             if (items[i] === mainArray[j]) {
+//                 mainArray.splice(j, 1);
+//             }
+//         }
+//     }   
+//     return mainArray;
+//     };
 
-    let result = removeFromArray([2,4,10], 10);
-console.log(result);
+//     let result = removeFromArray([2,4,10], 10);
+// console.log(result);
 
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
+
+
+function removeFromArray(...args) {
+    let mainArray = args[0];
+    console.log(mainArray);
+
+    let filtered = mainArray.filter(nums => !args.includes(nums));
+return filtered;
+}
+
+let result = removeFromArray([1,2,3,4], 3, 4);
+console.log(result);
