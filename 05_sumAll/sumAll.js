cbbaee8f90529e18647d1dc0a56f1a26ee6e23f4
Evan Buckmaster <evanbuckmaster@gmail.com>
2022-03-24 12:22:44 -0400

    completed 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((!Number.isInteger(num1)) || (!Number.isInteger(num2))) {
+        return 'ERROR';
+    } else if ((num1 < 0) || (num2 < 0)) {
+        return 'ERROR';
+    }
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    let finalSum =0;
+    
+    for (let i = min; i <= max; i++) {
+        finalSum += i;
+    }
+return finalSum;
 };
+
+let result = sumAll(1, 4);
+console.log(result);
 
 // Do not edit below this line
 module.exports = sumAll;
