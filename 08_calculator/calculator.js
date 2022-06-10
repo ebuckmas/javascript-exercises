70414795d31dd0f384bbf54a3c3b3975d0e4688f
Evan Buckmaster <evanbuckmaster@gmail.com>
2022-06-10 11:53:04 -0400

    completed 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, num) => acc * num);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0)  {
+    return 1;
+  } else if (num < 0) {
+    return -1;
+  } else {
+    return (num * factorial(num - 1));
+  }
 };
-
+console.log(factorial(10));
 // Do not edit below this line
 module.exports = {
   add,
