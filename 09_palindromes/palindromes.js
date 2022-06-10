9396347a7ff579c1e8bd68af2ab5593de9f85bc1
Evan Buckmaster <evanbuckmaster@gmail.com>
2022-06-10 13:10:37 -0400

    completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanedString = string.replace(/\W+/g, '').toLowerCase();
+    const reversedString = string.split('').reverse().join('').replace(/\W+/g, '').toLowerCase();
+      
+    if (cleanedString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
+        
 };
 
 // Do not edit below this line
