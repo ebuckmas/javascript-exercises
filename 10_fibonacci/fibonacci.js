e9615cc8bc5c3e963ce8c163c8eb5a465009913b
Evan Buckmaster <evanbuckmaster@gmail.com>
2022-06-10 16:23:28 -0400

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let num = Number(number);
+    if (num < 0) {
+        return 'OOPS';
+    } else if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } 
+    return fibonacci(num - 2) + (fibonacci(num - 1));
+    
 };
+
+
+console.log(fibonacci(25));
+
+//fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
 
 // Do not edit below this line
 module.exports = fibonacci;
